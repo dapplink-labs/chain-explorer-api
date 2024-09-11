91f30b4793cc0dfd0c9e4b5370d630a37d87396c
guoshijiang <guoshijiang2012@163.com>
2024-09-11 18:50:16 +0800

    fix

--- examples/account_test.go
+++ examples/account_test.go
@@ -2,8 +2,9 @@ package tests
 
 import (
 	"fmt"
-	"github.com/dapplink-labs/chain-explorer-api/common/account"
 	"testing"
+
+	"github.com/dapplink-labs/chain-explorer-api/common/account"
 )
 
 func TestGetAccountBalance(t *testing.T) {
@@ -91,3 +92,59 @@ func TestTokenGetAccountBalance(t *testing.T) {
 	fmt.Println("======================")
 
 }
+
+//func TestGetMultiAccountBalance(t *testing.T) {
+//	oklinkClient, etherscanClient, err := NewMockClient()
+//	if err != nil {
+//		fmt.Println("new mock client fail", "err", err)
+//	}
+//	accountItem := []string{"0x4838B106FCe9647Bdf1E7877BF73cE8B0BAD5f97", "0x4E5B2e1dc63F6b91cb6Cd759936495434C7e972F"}
+//	symbol := []string{"ETH", "ETH"}
+//	contractAddress := []string{"0x00", "0x00"}
+//	protocolType := []string{"", ""}
+//	page := []string{"1", "1"}
+//	limit := []string{"10", "10"}
+//	acbr := &account.AccountBalanceRequest{
+//		ChainShortName:  "ETH",
+//		ExplorerName:    "oklink",
+//		Account:         accountItem,
+//		Symbol:          symbol,
+//		ContractAddress: contractAddress,
+//		ProtocolType:    protocolType,
+//		Page:            page,
+//		Limit:           limit,
+//	}
+//}
+
+//func TestTokenGetMultiAccountBalance(t *testing.T) {
+//	oklinkClient, etherscanClient, err := NewMockClient()
+//	if err != nil {
+//		fmt.Println("new mock client fail", "err", err)
+//	}
+//	accountItem := []string{"0x55FE002aefF02F77364de339a1292923A15844B8"}
+//	symbol := []string{"USDT"}
+//	contractAddress := []string{"0xdAC17F958D2ee523a2206206994597C13D831ec7"}
+//	protocolType := []string{"token_20"}
+//	page := []string{"1"}
+//	limit := []string{"10"}
+//	acbr := &account.AccountBalanceRequest{
+//		ChainShortName:  "ETH",
+//		ExplorerName:    "oklink",
+//		Account:         accountItem,
+//		Symbol:          symbol,
+//		ContractAddress: contractAddress,
+//		ProtocolType:    protocolType,
+//		Page:            page,
+//		Limit:           limit,
+//	}
+//	oklinkResp, err := oklinkClient.GetMultiAccountBalance(acbr)
+//	if err != nil {
+//		t.Error(err)
+//	}
+//
+//	etherscanResp, err := etherscanClient.GetMultiAccountBalance(acbr)
+//	if err != nil {
+//		t.Error(err)
+//	}
+//
+//}

70788c8247c4dd0074f8ed4b1f832069168c408c
leo.shi <554601845@qq.com>
2024-09-11 21:17:09 +0800

    feature: gas_fee

--- explorer/oklink/gas_fee.go
+++ explorer/oklink/gas_fee.go
@@ -1,6 +1,7 @@
 package oklink
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/dapplink-labs/chain-explorer-api/common/gas_fee"
@@ -8,12 +9,17 @@ import (
 
 func (cea *ChainExplorerAdaptor) GetEstimateGasFee(request *gas_fee.GasEstimateFeeRequest) (*gas_fee.GasEstimateFeeResponse, error) {
 	apiUrl := fmt.Sprintf("api/v5/explorer/blockchain/fee?chainShortName=%s", request.ChainShortName)
-	resp := &gas_fee.GasEstimateFeeResponse{}
+	var resp []gas_fee.GasEstimateFeeResponse
 
-	err := cea.baseClient.Call(ChainExplorerName, "", "", apiUrl, nil, resp)
+	err := cea.baseClient.Call(ChainExplorerName, "", "", apiUrl, nil, &resp)
 
 	if err != nil {
 		return nil, err
 	}
-	return resp, nil
+	// 检查切片的长度
+	if len(resp) == 0 {
+		return nil, errors.New("no data returned from API")
+	}
+
+	return &resp[0], nil
 }
